af83e5776223899666576fde6a695bef0d566153
miguelToscano <34905912+miguelToscano@users.noreply.github.com>
2018-04-26 15:51:10 -0300

    Add files via upload

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include "jugador.h"
+#include "setup.h"
+
+#endif
