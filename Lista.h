9c462bbcd37d96d182879211d2c2c1a4c340fa41
tomasLopezHidalgo <38630216+tomasLopezHidalgo@users.noreply.github.com>
2018-05-11 23:59:48 -0300

    Add files via upload

---
+++ Lista.h
@@ -0,0 +1,117 @@
+#ifndef SRC_LISTA_H_
+#define SRC_LISTA_H_
+
+#include <iostream>
+#include "Nodo.h"
+
+template < typename dato >
+class Lista {
+
+private:
+
+	int tamanio;
+
+	Nodo<dato>* primerNodo;
+
+public:
+
+	Lista();
+
+	void agregarElemento(dato datoNuevo);
+
+	Nodo<dato>* obtenerNodo(unsigned int posicion);
+
+	dato eliminarElemento(unsigned int posicion);
+
+	dato mostrarElemento(unsigned int posicion);
+
+	bool ListaVacia();
+
+	virtual ~Lista();
+
+};
+
+template < typename dato >
+Lista<dato>::Lista() {
+
+	this->primerNodo = 0;
+	this->tamanio = 0;
+}
+
+template < typename dato >
+void Lista<dato>::agregarElemento(dato datoNuevo){
+
+	Nodo<dato>* nuevoNodo = new Nodo<dato>;
+	Nodo<dato>* nodoAux = primerNodo;
+
+	if(this->ListaVacia()){
+		nuevoNodo->modificarDato(datoNuevo);
+		primerNodo = nuevoNodo;
+	}
+	else{
+		while(nodoAux->mostrarSiguiente() != NULL)
+			nodoAux = nodoAux->mostrarSiguiente();
+		nodoAux->modificarSiguiente(nuevoNodo);
+		nuevoNodo->modificarDato(datoNuevo);
+	}
+	this->tamanio++;
+}
+
+template < typename dato >
+Nodo<dato>* Lista<dato>::obtenerNodo(unsigned int posicion){
+
+	Nodo<dato>* nodoAux = this->primerNodo;
+
+	for(unsigned int i = 0; i < posicion; i++)
+		nodoAux = nodoAux->mostrarSiguiente();
+
+	return nodoAux;
+}
+
+template < typename dato >
+dato Lista<dato>::eliminarElemento(unsigned int posicion){
+
+	posicion = posicion - 1;
+
+	Nodo<dato>* nodoAux = obtenerNodo(posicion);
+
+	if(posicion == 0)
+
+		primerNodo = nodoAux->mostrarSiguiente();
+	else{
+
+		Nodo<dato>* nodoAnterior = obtenerNodo(posicion - 1);
+		nodoAnterior->modificarSiguiente(nodoAux->mostrarSiguiente());
+	}
+
+	this->tamanio--;
+
+	dato ultimoDato = nodoAux->mostrarDato();
+
+	delete nodoAux;
+
+	return ultimoDato;
+}
+
+template < typename dato >
+dato Lista<dato>::mostrarElemento(unsigned int posicion){
+
+	Nodo<dato>* nodoAux = obtenerNodo(posicion);
+
+	return nodoAux->mostrarDato();
+}
+
+template < typename dato >
+bool Lista<dato>::ListaVacia(){
+
+	return (this->primerNodo == 0);
+}
+
+template < typename dato >
+Lista<dato>::~Lista() {
+	int i = 0;
+	while(!ListaVacia())
+		eliminarElemento(i + 1);
+}
+
+#endif /* SRC_LISTA_H_ */
---
+++ Nodo.h
@@ -0,0 +1,64 @@
+#ifndef SRC_NODO_H_
+#define SRC_NODO_H_
+
+template < typename dato >
+class Nodo {
+
+private:
+
+	dato datoDelNodo;
+
+	Nodo<dato>* siguienteNodo;
+
+public:
+
+	Nodo();
+
+	void modificarSiguiente(Nodo<dato>* siguienteNodo);
+
+	void modificarDato(dato datoNuevo);
+
+	dato mostrarDato();
+
+	Nodo<dato>* mostrarSiguiente();
+
+	virtual ~Nodo();
+
+};
+
+template < typename dato >
+Nodo<dato>::Nodo(){
+
+	this->siguienteNodo = 0;
+}
+
+template < typename dato >
+void Nodo<dato>::modificarSiguiente(Nodo<dato>* siguienteNodo){
+
+	this->siguienteNodo = siguienteNodo;
+}
+
+template < typename dato >
+void Nodo<dato>::modificarDato(dato datoNuevo){
+
+	this->datoDelNodo = datoNuevo;
+}
+
+template < typename dato >
+dato Nodo<dato>::mostrarDato(){
+
+	return this->datoDelNodo;
+}
+
+template < typename dato >
+Nodo<dato>* Nodo<dato>::mostrarSiguiente(){
+
+	return this->siguienteNodo;
+}
+
+template < typename dato >
+Nodo<dato>::~Nodo(){
+
+}
+
+#endif
