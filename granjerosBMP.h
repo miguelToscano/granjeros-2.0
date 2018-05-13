feef0b7778c26db184041b9209b3668bffecc5a0
triniMamud <38739652+triniMamud@users.noreply.github.com>
2018-05-13 16:12:51 -0300

    Add files via upload

--- granjerosBMP.h
+++ granjerosBMP.h
@@ -8,23 +8,35 @@
 #include "EasyBMP.h"
 
 #ifndef GRANJEROSBMP_H_
+
 #define GRANJEROSBMP_H_
 
 
 class GranjerosBMP{
 
 	private:
+
 		RGBApixel marronDisponible;
+
 		RGBApixel verdeBordes;
+
 		RGBApixel rojoA;
+
 		RGBApixel amarilloB;
+
 		RGBApixel azulC;
 
 
+
 	public:
+
 		GranjerosBMP();
+
 		void pintarFondo (BMP imagen);
+
 		void pintarBordesExteriores(BMP imagen);
+
+		void pintarBordesInternos (BMP imagen, int fila, int columna);
 
 
 };
