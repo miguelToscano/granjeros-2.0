31ad2d83dc8b016c798a340c09cf93a972f9e0aa
triniMamud <38739652+triniMamud@users.noreply.github.com>
2018-05-22 20:27:25 -0300

    Add files via upload

---
+++ Campo.h
@@ -0,0 +1,63 @@
+/*
+ * Campo.h
+ *
+ *  Created on: 11 may. 2018
+ *      Author: tomas
+ */
+
+#ifndef CAMPO_H_
+#define CAMPO_H_
+
+#include "parcela.h"
+#include "Lista.h"
+#include "Nodo.h"
+#include <iostream>
+
+const float FACTOR_PRECIO_TERRENO = 1.25;
+
+class Campo {
+
+private:
+
+	Lista <Parcela**> ListaDeTerrenos;
+	int cantidadTerrenos;
+	unsigned int filas;
+	unsigned int columnas;
+
+	int precioTerreno; // Creditos por terreno
+
+public:
+
+	Campo();
+
+	Campo(unsigned int filas,unsigned int columnas);
+
+	void agregarTerreno();
+
+	void establecerFilas(int filas);
+
+	void establecerColumnas(int columnas);
+
+	void crearCampo();
+
+	void mostrarCampo();
+
+	void mostrarTerreno(Parcela** terreno);
+
+	int obtenerCantidadTerrenos();
+
+	void eliminarTerreno(unsigned int posicion);
+
+	int obtenerFilas();
+
+	int obtenerColumnas();
+
+	int obtenerPrecioTerreno();
+
+	void actualizarPrecioTerreno();
+
+	virtual ~Campo();
+
+};
+
+#endif /* SRC_CAMPO_H_ */
