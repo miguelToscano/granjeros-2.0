67ab21fa1fc9e28dece6bb15de705aaaba5bcf5e
tomasLopezHidalgo <38630216+tomasLopezHidalgo@users.noreply.github.com>
2018-05-25 22:18:07 -0300

    Add files via upload

--- Almacen.cpp
+++ Almacen.cpp
@@ -13,70 +13,79 @@ Almacen::Almacen(){
 	this->capacidadMaxima = 0;
 }
 
-
 Almacen::Almacen(const int N, const int M){
-	int capacidad = 2 * (N + M);
-	this->capacidadMaxima = capacidad;
+
+	this->capacidadMaxima = 2 * (N + M);
 }
 
 bool Almacen::agregarCosechaAlmacen(Cultivo cosecha){
+
+	bool fueAgregado = false;
 	if(hayLugar()){
+
 		this->ListaDeCosechas.agregarElemento(cosecha);
-		return true;
-	}else{
-		return false;
+		fueAgregado = true;
 	}
+	return fueAgregado;
 }
 
-int Almacen::despacharCosechas(){
-	cout << "hasta aca llega 1";
-	this->ListaDeCosechas.iniciarCursor();
-	cout << "hasta aca llega 2";
-	int rentabilidad = 0;
-	int ganancias = 0;
-	int i = 1;
-	cout << "hasta aca llega";
-	while(ListaDeCosechas.avanzarCursor()){
-		Cultivo cultivo = ListaDeCosechas.obtenerCursor();
-		rentabilidad = cultivo.obtenerRentabilidad();
-		cout << "rentabilidad =" << cultivo.obtenerRentabilidad();
-		Destino DestinoCosecha(cultivo.obtenerTipo());
-		cout << "costo envio" << DestinoCosecha.mostrarCostoEnvio();
-		ganancias += (rentabilidad - DestinoCosecha.mostrarCostoEnvio());
-		cout << i;
+int Almacen::despacharCosecha(Cultivo cosecha){
 
+	bool cosechaEncontrada = false;
+	int posicion = 1;
+	Destino destinoCosecha(cosecha.obtenerTipo());
+
+	if( destinoCosecha.mostrarNombreDestino() == DESTINO_INEXISTENTE )
+		throw string("ERROR: Semilla inexistente en destinos");
+
+	ListaDeCosechas.iniciarCursor();
+	while(ListaDeCosechas.avanzarCursor()){
+		if(ListaDeCosechas.obtenerCursor().obtenerTipo() == cosecha.obtenerTipo())
+			cosechaEncontrada = true;
+
+		posicion++;
 	}
-	ListaDeCosechas.eliminarElementos();
-	cout << this->ListaDeCosechas.contarElementos();
-	cout << "ganancias =" << ganancias;
-	return ganancias;
+	if( !cosechaEncontrada )
+		throw string("ERROR: Cosecha no pertenece al almacén");
+
+	ListaDeCosechas.eliminarElemento(posicion);
+	return (cosecha.obtenerRentabilidad() - destinoCosecha.mostrarCostoEnvio());
 }
 
 void Almacen::aumentarCapacidad(unsigned int cantidadDeLugares){
 
 	this->capacidadMaxima += cantidadDeLugares;
-
 }
 
 int Almacen::obtenerCantidadDeCosechas(){
 
 	return ListaDeCosechas.obtenerTamanio();
-
 }
 
 int Almacen::obtenerCapacidadMaxima(){
+
 	return capacidadMaxima;
 }
 
+Cultivo Almacen::obtenerElementoDePosicion(unsigned int posicion){
+	if(posicion < 1 || posicion > this->obtenerCantidadDeCosechas())
+
+		throw string("Posicion invalida de almacen");
+	return this->ListaDeCosechas.mostrarElemento(posicion);
+}
+
 bool Almacen::hayLugar(){
+
 	return (capacidadMaxima > obtenerCantidadDeCosechas());
 }
 
 bool Almacen::estaVacio(){
 
-	return this->ListaDeCosechas.estaVacia();
+	return obtenerCantidadDeCosechas();
 }
+
 Almacen::~Almacen(){
-	this->ListaDeCosechas.eliminarElementos();
+
+	this->ListaDeCosechas.~Lista();
 }
 
